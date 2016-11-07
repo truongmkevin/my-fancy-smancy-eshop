4ab51802392e3edfcd6f8d4ab88a5475306c9cd8
KT <truongmkevin@yahoo.com>
2016-11-07 16:36:11 +0000

    added paperclip to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107144537) do
+ActiveRecord::Schema.define(version: 20161107163501) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -26,10 +29,14 @@ ActiveRecord::Schema.define(version: 20161107144537) do
     t.text     "description"
     t.integer  "quantity"
     t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
-  add_index "products", ["category_id"], name: "index_products_on_category_id"
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
 end
