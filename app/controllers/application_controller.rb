ac9ca54f44d95012ecdd6606f4639f8a2a346790
KT <truongmkevin@yahoo.com>
2016-11-10 06:17:43 +0000

    added auth_users and created sign out btn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :categories
+  before_action :authenticate_user!
 
   def categories
     @categories = Category.all
