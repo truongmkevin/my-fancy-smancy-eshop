d9973e1f885544e69f460346e4319e0cde85ecdf
KT <truongmkevin@yahoo.com>
2016-11-14 16:28:24 +0000

    added checkout functionality and page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :categories
-  before_action :authenticate_user!
 
   def categories
     @categories = Category.all
--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,9 +1,35 @@
 class CartController < ApplicationController
+
+  before_action :authenticate_user!, only: [:checkout]
+
+  LOCAL_SALES_TAX = 0.0925
+
   def view_cart
     @line_items = LineItem.all
   end
 
   def checkout
+
+    @line_items = LineItem.all
+    @order = Order.new
+    @order.user_id = current_user.id
+    sum = 0
+
+    @line_items.each do |line_item|
+      sum += line_item.total
+
+      # @order.order_items[line_item.product_id] = line_item.quantity
+
+      line_item.product.quantity -= line_item.quantity
+      line_item.product.save
+    end
+
+    @order.subtotal = sum
+    @order.sales_tax = sum * LOCAL_SALES_TAX
+    @order.grand_total = @order.subtotal + @order.sales_tax
+    @order.save
+
+    LineItem.destroy_all
   end
 
   def add_to_cart
